0927e8b869760e9276a688b72de733e1838ca084
Ellis Trickett <ellis.trickett@gmail.com>
2020-04-06 18:40:51 +0100

    Added a my_puts method to align all strings

--- directory.rb
+++ directory.rb
@@ -1,10 +1,14 @@
+def my_puts(str)
+  puts str.center(72)
+end
+
 def input_students
   students = []
-  puts "Enter name of student you would like to add".center(72)
+  my_puts "Enter name of student you would like to add"
   name = gets.chomp.capitalize
   while !name.empty? do
     while true do
-    puts "What cohort is #{name} in? If #{name} does not have one yet, just press return...".center(72)
+    my_puts "What cohort is #{name} in? If #{name} does not have one yet, just press return..."
     cohort = gets.chomp.capitalize
     if cohort == nil
       cohort = "N/A"
@@ -16,39 +20,39 @@ def input_students
       break
     end
   end
-    puts "What is #{name}'s favourite hobbie?".center(72)
+    my_puts "What is #{name}'s favourite hobbie?"
     hobbie = gets.chomp.capitalize
-    puts "What is #{name}'s date of birth?".center(72)
+    my_puts "What is #{name}'s date of birth?"
     dob = gets.chomp.capitalize
-    puts "What is #{name}'s height?".center(72)
+    my_puts "What is #{name}'s height?"
     height = gets.chomp.capitalize
     students << {name: name.to_sym, cohort: cohort.to_sym,
     hobbie: hobbie.to_sym, date_of_birth: dob.to_sym, height: height.to_sym}
-    puts "Now we have #{students.count} students".center(72)
-    puts "If you have finished, just hit return twice".center(72)
-    puts "Enter another students name".center(72)
+    my_puts "Now we have #{students.count} students"
+    my_puts "If you have finished, just hit return twice"
+    my_puts "Enter another students name"
     name = gets.chomp.capitalize
   end
   students
 end
 
 def print_header
-  puts "The students of Villains Academy".center(72)
-  puts "-------------".center(72)
+  my_puts "The students of Villains Academy"
+  my_puts "-------------"
 end
 def print(students)
   count = students.length
   until count == 0 do
     students.each.with_index(1) do |student, index|
       if student[:name].length < 12
-        puts "#{index}. #{student[:name]} (#{student[:cohort]} Cohort, #{student[:hobbie]} Hobbie, #{student[:date_of_birth]} DOB, #{student[:height]} Height)".center(72)
+        my_puts "#{index}. #{student[:name]} (#{student[:cohort]} Cohort, #{student[:hobbie]} Hobbie, #{student[:date_of_birth]} DOB, #{student[:height]} Height)"
         count -= 1
       end
     end
   end
 end
 def print_footer(names)
-  puts "Overall, we have #{names.count} great students".center(72)
+  my_puts "Overall, we have #{names.count} great students"
 end
 students = input_students
 print_header
