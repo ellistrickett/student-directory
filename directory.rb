16731bb903e099b5c038fb091092cb7c0be76e5f
Ellis Trickett <ellis.trickett@gmail.com>
2020-04-06 18:30:18 +0100

    Ask for cohort, if empty provide a default value, if mispelt repeat question and finally assign cohort as a symbol

--- directory.rb
+++ directory.rb
@@ -3,14 +3,27 @@ def input_students
   puts "Enter name of student you would like to add".center(72)
   name = gets.chomp.capitalize
   while !name.empty? do
+    while true do
+    puts "What cohort is #{name} in? If #{name} does not have one yet, just press return...".center(72)
+    cohort = gets.chomp.capitalize
+    if cohort == nil
+      cohort = "N/A"
+      break
+    elsif cohort == "January" || cohort == "Febuary" || cohort == "March" ||
+      cohort == "April" || cohort == "May" || cohort == "June" ||
+      cohort == "July" || cohort == "August" || cohort == "September" ||
+      cohort == "October" || cohort == "November" || cohort == "December"
+      break
+    end
+  end
     puts "What is #{name}'s favourite hobbie?".center(72)
     hobbie = gets.chomp.capitalize
     puts "What is #{name}'s date of birth?".center(72)
     dob = gets.chomp.capitalize
     puts "What is #{name}'s height?".center(72)
     height = gets.chomp.capitalize
-    students << {name: name, cohort: :November, hobbie: hobbie, date_of_birth: dob,
-    height: height}
+    students << {name: name.to_sym, cohort: cohort.to_sym,
+    hobbie: hobbie.to_sym, date_of_birth: dob.to_sym, height: height.to_sym}
     puts "Now we have #{students.count} students".center(72)
     puts "If you have finished, just hit return twice".center(72)
     puts "Enter another students name".center(72)
