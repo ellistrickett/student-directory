b0a20a06e39a5add2fb4380e2e053990c358395c
Ellis Trickett <ellis.trickett@gmail.com>
2020-04-07 14:21:51 +0100

    Refactored interactive menu into multiple methods

--- directory.rb
+++ directory.rb
@@ -1,28 +1,35 @@
+@students =  []
+
 def interactive_menu
-  students = []
   loop do
-  # 1. print the menu and ask the user what to do
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit"
-  # 2. read the input and save it into a variable
-    selection = gets.chomp
-  # 3. do what the user has asked
-    case selection
+    print_menu
+    process(gets.chomp)
+  end
+end
+
+def process(selection)
+  case selection
     when "1"
-      students = input_students
+      input_students
     when "2"
-    # show the students
-      print_header
-      print(students)
-      print_footer(students)
+      show_students
     when "9"
-      exit # this will cause the program to exit
+      exit
     else
       puts "I don't know what you meant, try again"
-  # 3. do what the user has asked
     end
   end
+
+def show_students
+  print_header
+  print_students_list
+  print_footer
+end
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
 end
 
 def my_puts(str)
@@ -30,7 +37,6 @@ def my_puts(str)
 end
 
 def input_students
-  students = []
   my_puts "Enter name of student you would like to add"
   name = gets.tr("\n\r", "").capitalize
   while !name.empty? do
@@ -53,44 +59,42 @@ def input_students
     dob = gets.chomp.capitalize
     my_puts "What is #{name}'s height?"
     height = gets.chomp.capitalize
-    students << {name: name.to_sym, cohort: cohort.to_sym,
+    @students << {name: name.to_sym, cohort: cohort.to_sym,
     hobbie: hobbie.to_sym, date_of_birth: dob.to_sym, height: height.to_sym}
-    if students.count == 1
+    if @students.count == 1
       my_puts "Now we have 1 student"
     else
-      my_puts "Now we have #{students.count} students"
+      my_puts "Now we have #{@students.count} students"
     end
     my_puts "If you have finished, just hit return twice"
     my_puts "Enter another students name"
     name = gets.chomp.capitalize
   end
-  students
+  @students
 end
 
 def print_header
   my_puts "The students of Villains Academy"
   my_puts "-------------"
 end
-def print(students)
+def print_students_list
   cohorts = []
-  students.each {|student| cohorts.push(student[:cohort])}
+  @students.each {|student| cohorts.push(student[:cohort])}
   cohorts.uniq.each do |cohort|
   my_puts "Cohort: #{cohort}"
-  students.map.with_index(1) do |student, index|
+  @students.map.with_index(1) do |student, index|
     my_puts "#{index}. #{student[:name]} (#{student[:hobbie]} Hobbie, #{student[:date_of_birth]} DOB, #{student[:height]} Height)" if student [:cohort] == cohort
     end
   end
 end
 
-def print_footer(names)
-  my_puts "Overall, we have #{names.count} great students"
+def print_footer
+  my_puts "Overall, we have #{@students.count} great students"
 end
 
 interactive_menu
-if students.length > 0
-  print_header
-  print(students)
-  print_footer(students)
+if @students.length > 0
+  show_students
 else
   my_puts "There is currently no students at Villains Academy"
 end
